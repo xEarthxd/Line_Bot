f5ae0f4da6431afc536e73275595db1365302308
xEarthxd <di_stat@hotmail.com>
2019-09-17 14:20:02 +0700

    del controller for message test

--- src/main/java/Application/Controller.java
+++ src/main/java/Application/Controller.java
@@ -1,15 +1,15 @@
-package Application;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/")
-public class Controller {
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String helloWorld() {
-        return "Hello World";
-    }
-}
+//package Application;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/")
+//public class Controller {
+//
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String helloWorld() {
+//        return "Hello World";
+//    }
+//}
