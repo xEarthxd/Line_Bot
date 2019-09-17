729694fd565d141b0c9b610873dfa7624e4b8ebf
xEarthxd <di_stat@hotmail.com>
2019-09-17 12:00:14 +0700

    mv Application to /Application

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -1,3 +1,5 @@
+package Application;
+
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
